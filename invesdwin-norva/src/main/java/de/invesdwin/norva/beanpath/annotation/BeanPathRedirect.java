8f452589920f3e8a4609b9b514b00b0b2a829598
subes <gsubes@gmail.com>
2016-02-21 00:14:42 +0100

    more javadoc

--- invesdwin-norva/src/main/java/de/invesdwin/norva/beanpath/annotation/BeanPathRedirect.java
+++ invesdwin-norva/src/main/java/de/invesdwin/norva/beanpath/annotation/BeanPathRedirect.java
@@ -6,7 +6,10 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * Can be used to change the tree structure via a redirect in the bean path.
+ * Can be used to change the tree structure via a redirect in the bean path. Quite useful when you want to modify
+ * elements of a subtype but cannot add annotations to it (e.g. since it is a database class you want to reuse from a
+ * different module that has no dependency to invesdwin-nowicket). One example is to add the Hidden annotation to an
+ * element of it or add a disable or validate utility method from the outside.
  * 
  * @author subes
  *
@@ -16,7 +19,8 @@ import java.lang.annotation.Target;
 public @interface BeanPathRedirect {
 
     /**
-     * The path that should be intercepted to be redirected into this element.
+     * The path that should be intercepted to be redirected into this element. The bean path is specified relative from
+     * the current element.
      */
     String value();
 
