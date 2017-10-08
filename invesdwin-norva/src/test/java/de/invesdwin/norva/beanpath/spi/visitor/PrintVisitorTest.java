63b6c966ab3ae14c41fa175977d490c1bd747a7b
subes <gsubes@gmail.com>
2017-10-08 23:17:05 +0200

    dependency updates and some java9 preparations

--- invesdwin-norva/src/test/java/de/invesdwin/norva/beanpath/spi/visitor/PrintVisitorTest.java
+++ invesdwin-norva/src/test/java/de/invesdwin/norva/beanpath/spi/visitor/PrintVisitorTest.java
@@ -2,13 +2,16 @@ package de.invesdwin.norva.beanpath.spi.visitor;
 
 import javax.annotation.concurrent.NotThreadSafe;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.runner.RunWith;
 
 import de.invesdwin.norva.beanpath.impl.clazz.BeanClassContainer;
 import de.invesdwin.norva.beanpath.impl.clazz.BeanClassContext;
 import de.invesdwin.norva.beanpath.impl.clazz.BeanClassProcessor;
 import de.invesdwin.norva.beanpath.impl.clazz.BeanClassType;
 
+@RunWith(JUnitPlatform.class)
 @NotThreadSafe
 public class PrintVisitorTest {
 
