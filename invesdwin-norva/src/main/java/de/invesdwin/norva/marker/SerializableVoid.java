73f6b16ab245e396a8e8b196cc867c963a80402f
subes <gsubes@gmail.com>
2020-01-13 15:39:03 +0100

    void

--- invesdwin-norva/src/main/java/de/invesdwin/norva/marker/SerializableVoid.java
+++ invesdwin-norva/src/main/java/de/invesdwin/norva/marker/SerializableVoid.java
@@ -1,9 +1,17 @@
 package de.invesdwin.norva.marker;
 
+import java.io.Serializable;
+
 import javax.annotation.concurrent.Immutable;
 
+/**
+ * Not a ISerializableValueObject because it will never be serialized anyway.
+ * 
+ * This class can be used in interfaces where a serializable generic is required but none will be used. Often it is
+ * better to redesign the interface, but if that is not possible, this type might provide a workaround.
+ */
 @Immutable
-public final class SerializableVoid implements ISerializableValueObject {
+public final class SerializableVoid implements Serializable {
 
     /*
      * The Void class cannot be instantiated.
