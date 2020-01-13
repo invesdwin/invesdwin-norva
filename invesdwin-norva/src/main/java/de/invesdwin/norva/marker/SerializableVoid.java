318d94ec3603a3f4d708a12d943046384a8fc50f
subes <gsubes@gmail.com>
2020-01-13 15:39:24 +0100

    text

--- invesdwin-norva/src/main/java/de/invesdwin/norva/marker/SerializableVoid.java
+++ invesdwin-norva/src/main/java/de/invesdwin/norva/marker/SerializableVoid.java
@@ -5,7 +5,7 @@ import java.io.Serializable;
 import javax.annotation.concurrent.Immutable;
 
 /**
- * Not a ISerializableValueObject because it will never be serialized anyway.
+ * Not a ISerializableValueObject because it will never be serialized anyway, thus no FST registration required.
  * 
  * This class can be used in interfaces where a serializable generic is required but none will be used. Often it is
  * better to redesign the interface, but if that is not possible, this type might provide a workaround.
