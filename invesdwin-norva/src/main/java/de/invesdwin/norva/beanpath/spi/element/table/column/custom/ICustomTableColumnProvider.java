541ced80aa30fd6c2913f250feb9f5d97b58ab85
subes <gsubes@gmail.com>
2021-02-15 21:40:59 +0100

    docs

--- invesdwin-norva/src/main/java/de/invesdwin/norva/beanpath/spi/element/table/column/custom/ICustomTableColumnProvider.java
+++ invesdwin-norva/src/main/java/de/invesdwin/norva/beanpath/spi/element/table/column/custom/ICustomTableColumnProvider.java
@@ -5,8 +5,14 @@ import java.util.List;
 import de.invesdwin.norva.beanpath.spi.element.table.ATableBeanPathElement;
 import de.invesdwin.norva.beanpath.spi.element.table.column.ITableColumnBeanPathElement;
 
+/**
+ * Implement this interface in the class that contains a table property to customize the columns flexibly.
+ */
 public interface ICustomTableColumnProvider {
 
+    /**
+     * Return a new list with added instances of ACustomTableColumn or reorder/filter existing columns in the new list.
+     */
     List<ITableColumnBeanPathElement> customize(ATableBeanPathElement tableElement,
             List<ITableColumnBeanPathElement> orderedColumnsUnmodifiable);
 
