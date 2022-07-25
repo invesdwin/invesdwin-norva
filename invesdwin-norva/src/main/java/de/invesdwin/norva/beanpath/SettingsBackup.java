93d43c88f7eb8f494a2f0bfa1787544a12c841d3
Edwin Stang <gsubes@gmail.com>
2022-07-25 18:53:12 +0200

    PER-225 settings backup

---
+++ invesdwin-norva/src/main/java/de/invesdwin/norva/beanpath/SettingsBackup.java
@@ -0,0 +1,81 @@
+package de.invesdwin.norva.beanpath;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.annotation.concurrent.NotThreadSafe;
+
+import de.invesdwin.norva.beanpath.impl.clazz.BeanClassProcessor;
+import de.invesdwin.norva.beanpath.impl.clazz.BeanClassProcessorConfig;
+import de.invesdwin.norva.beanpath.spi.element.IPropertyBeanPathElement;
+import de.invesdwin.norva.beanpath.spi.visitor.SimpleBeanPathVisitorSupport;
+
+@NotThreadSafe
+public class SettingsBackup {
+
+    private final Map<String, Object> backup = new HashMap<>();
+
+    public Object put(final String key, final Object value) {
+        return backup.put(key, value);
+    }
+
+    @SuppressWarnings("unchecked")
+    public <T> T get(final String key) {
+        return (T) backup.get(key);
+    }
+
+    public void backupPropertiesShallow(final Object obj) {
+        BeanClassProcessor.process(new BeanClassProcessorConfig(obj.getClass()).setShallowOnly(),
+                new SimpleBeanPathVisitorSupport() {
+                    @Override
+                    public void visitProperty(final IPropertyBeanPathElement e) {
+                        if (e.getAccessor().hasPublicSetterOrField()) {
+                            final Object value = e.getModifier().getValueFromRoot(obj);
+                            backup.put(obj.getClass().getSimpleName() + "_" + e.getBeanPath(), value);
+                        }
+                    }
+                });
+    }
+
+    public void restorePropertiesShallow(final Object obj) {
+        BeanClassProcessor.process(new BeanClassProcessorConfig(obj.getClass()).setShallowOnly(),
+                new SimpleBeanPathVisitorSupport() {
+                    @Override
+                    public void visitProperty(final IPropertyBeanPathElement e) {
+                        if (e.getAccessor().hasPublicSetterOrField()) {
+                            final Object value = backup.get(obj.getClass().getSimpleName() + "_" + e.getBeanPath());
+                            e.getModifier().setValueFromRoot(obj, value);
+                        }
+                    }
+                });
+    }
+
+    public void backupProperties(final Object obj) {
+        BeanClassProcessor.process(new BeanClassProcessorConfig(obj.getClass()), new SimpleBeanPathVisitorSupport() {
+            @Override
+            public void visitProperty(final IPropertyBeanPathElement e) {
+                if (e.getAccessor().hasPublicSetterOrField()) {
+                    final Object value = e.getModifier().getValueFromRoot(obj);
+                    backup.put(obj.getClass().getSimpleName() + "_" + e.getBeanPath(), value);
+                }
+            }
+        });
+    }
+
+    public void restoreProperties(final Object obj) {
+        BeanClassProcessor.process(new BeanClassProcessorConfig(obj.getClass()), new SimpleBeanPathVisitorSupport() {
+            @Override
+            public void visitProperty(final IPropertyBeanPathElement e) {
+                if (e.getAccessor().hasPublicSetterOrField()) {
+                    final Object value = backup.get(obj.getClass().getSimpleName() + "_" + e.getBeanPath());
+                    e.getModifier().setValueFromRoot(obj, value);
+                }
+            }
+        });
+    }
+
+    public void clear() {
+        backup.clear();
+    }
+
+}
