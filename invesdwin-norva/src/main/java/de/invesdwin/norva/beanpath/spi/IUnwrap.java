f790f54725f58710337876abf4066f56ee4c62cf
Edwin Stang <gsubes@gmail.com>
2024-12-24 12:36:38 +0100

    PER-894 don't use IUnwrap.unwrap in getters as it is expensive

--- invesdwin-norva/src/main/java/de/invesdwin/norva/beanpath/spi/IUnwrap.java
+++ invesdwin-norva/src/main/java/de/invesdwin/norva/beanpath/spi/IUnwrap.java
@@ -4,6 +4,10 @@ public interface IUnwrap {
 
     <T> T unwrap(Class<T> type);
 
+    /**
+     * WARNING: don't use this method where lots of calls are made since this can be costly. Instead use direct casting
+     * or store the unwrapped reference additionally in a variable/field.
+     */
     static <T> T unwrap(final IUnwrap obj, final Class<T> type) {
         if (obj == null) {
             return null;
